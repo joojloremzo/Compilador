9209829a3b7a3e4c07f78ef47be9f521327a9d08
Lorenzo Zoboli <82813315+joojloremzo@users.noreply.github.com>
2026-04-06 18:16:43 -0300

    Versão já com analisador léxico implementado

---
+++ Compilador/src/main/java/compilador_pck/SyntaticError.java
@@ -0,0 +1,14 @@
+package compilador_pck;
+
+public class SyntaticError extends AnalysisError
+{
+    public SyntaticError(String msg, int position)
+	 {
+        super(msg, position);
+    }
+
+    public SyntaticError(String msg)
+    {
+        super(msg);
+    }
+}
